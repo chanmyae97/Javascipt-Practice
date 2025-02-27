b81160eea8f49411b6a9b53032cd7ac76dfa58a3
Aung Chan Myae <aungchanmyae10997@gmail.com>
2025-02-27 17:30:31 +0700

    testing the namewith ai

--- Text Animation/index.js
+++ Text Animation/index.js
@@ -1,23 +1,38 @@
-const containerE1 = document.querySelector('.container')
+const containerE1 = document.querySelector('.container');
 
-const careers =[ "Software Engineer","Web Developer", 'Freelencer', 'Data Scientist' ];
+const careers = ["Software Engineer", "Web Developer", "Freelancer", "Data Scientist"];
 
 let careersIndex = 0;
-
 let charactersIndex = 0;
+let isDeleting = false;
+let typingSpeed = 100;
 
 updateText();
 
-function updateText(){
-    charactersIndex++;
-    containerE1.innerHTML = `<h1> I am a ${careers[careersIndex].slice(0,charactersIndex)} </h1>`;
-    if(charactersIndex === careers[careersIndex].length){
-        careersIndex++;
-        charactersIndex = 0;
+function updateText() {
+    const currentText = careers[careersIndex];
+    
+    if (!isDeleting) {
+        containerE1.innerHTML = `<h1>I am a ${currentText.substring(0, charactersIndex)}</h1>`;
+        charactersIndex++;
+        
+        if (charactersIndex > currentText.length) {
+            isDeleting = true;
+            typingSpeed = 50;
+            setTimeout(updateText, 1500); // Pause at the end of the word
+            return;
+        }
+    } else {
+        containerE1.innerHTML = `<h1>I am a ${currentText.substring(0, charactersIndex)}</h1>`;
+        charactersIndex--;
+        
+        if (charactersIndex === 0) {
+            isDeleting = false;
+            careersIndex = (careersIndex + 1) % careers.length;
+            typingSpeed = 100;
+        }
     }
-    if(careersIndex === careers.length){
-        careersIndex = 0;
-    }
-    setTimeout(updateText, 300);
+    
+    setTimeout(updateText, typingSpeed);
 }
 
